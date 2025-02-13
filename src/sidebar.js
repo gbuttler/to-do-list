d2c55278612491edd840eef835e56ee2c9a83964
Grace Buttler <grace.buttler@gmail.com>
2025-02-13 14:44:45 +0000

    now able to add projetcs to the form and the the html

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,21 +1,36 @@
-//new project button
+//array to hold projects
+const myProjects = ["Personal", "Work", "Hobbies"];
 
+//new project button
 const newProject = () => {
   console.log("creating a new project");
 
+  //using prompt to create a new project
+  let newProjectItem = prompt("Please enter your project name", "New Project");
+
+  //add projects to project array
+  function addNewProject() {
+    myProjects.push(newProjectItem);
+  }
+
   //add the new project to the html
-  let newProject = prompt("Please enter your project name", "New Project");
   let projectList = document.querySelector(".project-list");
   const para = document.createElement("p");
   para.classList.add("indv-project");
-  para.innerHTML = newProject;
+  para.innerHTML = newProjectItem;
   projectList.appendChild(para);
 
   //add project options to the form
   let formOption = document.getElementById("project");
   const option = document.createElement("option");
-  option.text = newProject;
+  option.text = newProjectItem;
   formOption.add(option);
+
+  //add new project to the array
+  addNewProject();
+
+  //show project list in console
+  console.log(myProjects);
 };
 
 export { newProject };
