110cd6abea78a766e878eaa4757b53e732c2d5d8
Grace Buttler <grace.buttler@gmail.com>
2025-02-19 13:55:35 +0000

    app functionality completed

--- src/projects.js
+++ src/projects.js
@@ -1,4 +1,4 @@
-import { myTasks } from "./task";
+import { myTasks, taskScreen } from "./task";
 
 //array to hold projects
 const myProjects = [];
@@ -59,6 +59,66 @@ const filterByProjects = (projName) => {
     return Task.project === projName;
   });
   console.log(filteredTasks);
+
+  let i = 0;
+  taskScreen.innerHTML = "";
+
+  filteredTasks.forEach((Task, j) => {
+    console.log(i, filteredTasks.length);
+    // single task div
+    let singleTask = document.createElement("div");
+    singleTask.className = "singleTask";
+    taskScreen.appendChild(singleTask);
+
+    //task name
+    let taskNamePara = document.createElement("p");
+    taskNamePara.className = "task-name";
+    taskNamePara.textContent = Task.taskName;
+    singleTask.appendChild(taskNamePara);
+
+    //project name
+    let projectNamePara = document.createElement("p");
+    projectNamePara.className = "task-project";
+    projectNamePara.textContent = Task.project;
+    singleTask.appendChild(projectNamePara);
+
+    //due date
+    let dueNamePara = document.createElement("p");
+    dueNamePara.className = "task-due";
+    dueNamePara.textContent = Task.due;
+    singleTask.appendChild(dueNamePara);
+
+    console.log(Task.due);
+
+    //notes div
+    let notesNamePara = document.createElement("p");
+    notesNamePara.className = "task-notes";
+    notesNamePara.textContent = Task.notes;
+    singleTask.appendChild(notesNamePara);
+
+    //create bottom task line div and append to single task
+    let bottomTaskLine = document.createElement("div");
+    bottomTaskLine.className = "bottom-task-line";
+    singleTask.appendChild(bottomTaskLine);
+
+    //complete task
+    let taskCompleteButton = document.createElement("button");
+    taskCompleteButton.className = "completed-status";
+    taskCompleteButton.addEventListener("click", () => {
+      console.log("task completed", j);
+      myTasks.splice(j, 1);
+      displayTasks();
+    });
+    let taskCompleteIcon = document.createElement("i");
+    taskCompleteIcon.className = "fa-solid fa-circle-check";
+    taskCompleteButton.appendChild(taskCompleteIcon);
+    bottomTaskLine.appendChild(taskCompleteButton);
+
+    console.log(`Task name is:${Task.taskName}`);
+    i++;
+
+    return singleTask;
+  });
 };
 
 export { myProjects, loadProjects, Project, filterByProjects, addNewProject };
